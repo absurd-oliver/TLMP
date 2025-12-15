7884ed1245472e5965bd6c281c350426b84f5f71
absurdoliver <166875516+absurd-oliver@users.noreply.github.com>
2025-12-15 13:40:46 -0500

    crashing tf out

--- js/tvcontrols.js
+++ js/tvcontrols.js
@@ -16,38 +16,34 @@ export async function episodeChange(event) {
   const maxEpisodes = await episodesAmount(els.show.value, els.season.value);
 
   let episodeChanged = false;
-  let maxorminEpreached = false;
 
   if (clickedButtonId === 'prevButton') { 
     if (episodeInput.value > 1) {
       episodeInput.value = parseInt(episodeInput.value) - 1;
       episodeChanged = true;
-    } else {
-      maxorminEpreached = true;
-    }
+    } 
   } else if (clickedButtonId === 'nextButton') {
     if (episodeInput.value < maxEpisodes){
       episodeInput.value = parseInt(episodeInput.value) + 1;
       episodeChanged = true;
-    } else {
-      maxorminEpreached = true;
     }
   }
 
   if (episodeChanged) {
     setTimeout(() => {
       start();
-      if(!maxorminEpreached) currentTargetElement.disabled = false;
+      currentTargetElement.disabled = false;
     }, 100);
-  } else if (maxorminEpreached) {
+  } else {
     currentTargetElement.disabled = false;
+    console.log(`Max episodes (${maxEpisodes}) reached or minimum reached.`);
   }
 }
 
 async function episodesAmount(title, season) {
     const apiKey = "d0004590";
     const apiRes = await fetch(
-        `https://www.omdbapi.com/?apikey=${apiKey}&t=${encodeURIComponent(title)}&Season=${season}`
+        `www.omdbapi.com{apiKey}&t=${encodeURIComponent(title)}&Season=${season}`
     );
     
     const data = await apiRes.json();
